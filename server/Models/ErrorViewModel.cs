3b99617a6cc4880e512b28229900905bdaeaef97
brandon Gulley <brandon.gulley511@gmail.com>
2019-11-10 12:58:36 -0700

    first commit

---
+++ server/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace wine_cellar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
